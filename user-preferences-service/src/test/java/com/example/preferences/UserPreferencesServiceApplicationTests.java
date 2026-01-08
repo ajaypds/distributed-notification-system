cbebac5ff8f4eaa655b7fe5a74240b1d0b3d22e3
Ajay Prasad <ajay.prasad@outlook.in>
2026-01-08 16:30:39 +0530

    added initial scaffold for user-preferences-service

---
+++ user-preferences-service/src/test/java/com/example/preferences/UserPreferencesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPreferencesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
