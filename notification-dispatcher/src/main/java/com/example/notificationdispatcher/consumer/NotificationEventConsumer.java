977c5436694e9be9bf8c62b0be33c762e3327177
Ajay Prasad <ajay.prasad@outlook.in>
2026-01-24 20:59:17 +0530

    updated flow for retry and dlq

--- notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/NotificationEventConsumer.java
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/NotificationEventConsumer.java
@@ -2,24 +2,47 @@ package com.example.notificationdispatcher.consumer;
 
 //import com.example.notificationdispatcher.event.NotificationEvent;
 import com.example.contract.NotificationEvent;
+import com.example.notificationdispatcher.exception.PermanentFailureException;
+import com.example.notificationdispatcher.exception.TransientFailureException;
+import com.example.notificationdispatcher.kafka.KafkaRetryConstants;
+import com.example.notificationdispatcher.kafka.RetryDlqPublisher;
 import com.example.notificationdispatcher.service.DispatcherService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class NotificationEventConsumer {
 
     private final DispatcherService dispatcherService;
+    private final RetryDlqPublisher retryDlqPublisher;
 
-//    public NotificationEventConsumer(DispatcherService dispatcherService) {
-//        this.dispatcherService = dispatcherService;
-//    }
+    @KafkaListener(topics = KafkaRetryConstants.MAIN_TOPIC)
+    public void consume(
+            NotificationEvent event,
+            @Header(
+                    value = KafkaRetryConstants.RETRY_COUNT_HEADER,
+                    required = false
+            ) Integer retryCount) {
 
-    @KafkaListener(topics = "notification-events")
-    public void consume(NotificationEvent event) {
-
-        dispatcherService.process(event);
+        int currentRetry = retryCount == null ? 0 : retryCount;
+        try{
+            log.info("Calling dispatcher service in NotificationEventConsumer to process event!");
+            dispatcherService.process(event);
+        } catch(TransientFailureException ex){
+            log.error("Transient failure occurred at NotificationEventConsumer");
+            if(currentRetry < KafkaRetryConstants.MAX_RETRIES){
+                retryDlqPublisher.publishToRetry(event, currentRetry + 1);
+            }else{
+                retryDlqPublisher.publishToDlq(event, "MAX_RETRIES_EXCEEDED");
+            }
+        }catch(PermanentFailureException ex){
+            log.error("Permanent failure occurred at NotificationEventConsumer");
+            retryDlqPublisher.publishToDlq(event, "PERMANENT FAILURE");
+        }
     }
 }
