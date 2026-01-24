9a8c93dbbdb7119310d6ad515c535ab9c98750f8
Ajay Prasad <ajay.prasad@outlook.in>
2026-01-24 21:04:37 +0530

    kafka listener for notification-events-retry topic

---
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/RetryEventConsumer.java
@@ -0,0 +1,39 @@
+package com.example.notificationdispatcher.consumer;
+
+import com.example.contract.NotificationEvent;
+import com.example.notificationdispatcher.kafka.KafkaRetryConstants;
+import com.example.notificationdispatcher.kafka.RetryDlqPublisher;
+import com.example.notificationdispatcher.service.DispatcherService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class RetryEventConsumer {
+
+    private final DispatcherService dispatcherService;
+    private final RetryDlqPublisher retryDlqPublisher;
+
+    @KafkaListener(topics = KafkaRetryConstants.RETRY_TOPIC)
+    public void retry(
+            NotificationEvent event,
+            @Header(KafkaRetryConstants.RETRY_COUNT_HEADER) int retryCount) {
+
+        try {
+            log.info("Calling dispatcher service in RetryEventConsumer to process event!");
+            dispatcherService.process(event);
+        }
+        catch (Exception ex) {
+            log.error("Exception failure occurred at RetryEventConsumer");
+            if (retryCount < KafkaRetryConstants.MAX_RETRIES) {
+                retryDlqPublisher.publishToRetry(event, retryCount + 1);
+            } else {
+                retryDlqPublisher.publishToDlq(event, "RETRY_EXHAUSTED");
+            }
+        }
+    }
+}
