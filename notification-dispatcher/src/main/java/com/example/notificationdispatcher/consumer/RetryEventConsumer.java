cdc04e664bcdd334ac660a56d2467d898b26d716
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-01 20:31:32 +0530

    added RetryEventConsumer

--- notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/RetryEventConsumer.java
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/RetryEventConsumer.java
@@ -1,6 +1,8 @@
 package com.example.notificationdispatcher.consumer;
 
 import com.example.contract.NotificationEvent;
+import com.example.notificationdispatcher.exception.PermanentFailureException;
+import com.example.notificationdispatcher.exception.TransientFailureException;
 import com.example.notificationdispatcher.kafka.KafkaRetryConstants;
 import com.example.notificationdispatcher.kafka.RetryDlqPublisher;
 import com.example.notificationdispatcher.service.DispatcherService;
@@ -21,19 +23,30 @@ public class RetryEventConsumer {
     @KafkaListener(topics = KafkaRetryConstants.RETRY_TOPIC)
     public void retry(
             NotificationEvent event,
-            @Header(KafkaRetryConstants.RETRY_COUNT_HEADER) int retryCount) {
+            @Header(KafkaRetryConstants.RETRY_COUNT_HEADER) int retryCount,
+            @Header(KafkaRetryConstants.NEXT_RETRY_AT_HEADER) long nextRetryAt) {
+
+        long now = System.currentTimeMillis();
+
+        if(now < nextRetryAt){
+            retryDlqPublisher.publishToRetry(event, retryCount);
+            return;
+        }
 
         try {
             log.info("Calling dispatcher service in RetryEventConsumer to process event!");
             dispatcherService.process(event);
         }
-        catch (Exception ex) {
+        catch (TransientFailureException ex) {
             log.error("Exception failure occurred at RetryEventConsumer");
             if (retryCount < KafkaRetryConstants.MAX_RETRIES) {
                 retryDlqPublisher.publishToRetry(event, retryCount + 1);
             } else {
                 retryDlqPublisher.publishToDlq(event, "RETRY_EXHAUSTED");
             }
+        }
+        catch(PermanentFailureException ex){
+            retryDlqPublisher.publishToDlq(event, "PERMANENT FAILURE");
         }
     }
 }
