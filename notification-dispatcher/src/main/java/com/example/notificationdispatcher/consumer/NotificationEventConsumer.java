0814f4980673ddb76057a83d907bed70393ecdec
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-02 20:38:11 +0530

    added logging in NotificationEventConsumer

--- notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/NotificationEventConsumer.java
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/consumer/NotificationEventConsumer.java
@@ -36,8 +36,10 @@ public class NotificationEventConsumer {
         } catch(TransientFailureException ex){
             log.error("Transient failure occurred at NotificationEventConsumer");
             if(currentRetry < KafkaRetryConstants.MAX_RETRIES){
+                log.info("Publishing event to retry topic from NotificationEventConsumer, currentRetry: {}", currentRetry);
                 retryDlqPublisher.publishToRetry(event, currentRetry + 1);
             }else{
+                log.info("Max retries exceeded, publishing event to DLQ from NotificationEventConsumer");
                 retryDlqPublisher.publishToDlq(event, "MAX_RETRIES_EXCEEDED");
             }
         }catch(PermanentFailureException ex){
