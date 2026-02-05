14fe8600656c8fdbc66ad9d5e57409cab0ee33d9
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-05 15:48:02 +0530

    added idempotency check function for email, sms and push individually

--- notification-dispatcher/src/main/java/com/example/notificationdispatcher/service/IdempotencyService.java
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/service/IdempotencyService.java
@@ -8,6 +8,9 @@ public class IdempotencyService {
 
     private final StringRedisTemplate redisTemplate;
     private static final String PROCESSED_KEY = "processed-events";
+    private static final String PROCESSED_EMAIL_KEY = "processed-email-events";
+    private static final String PROCESSED_SMS_KEY = "processed-sms-events";
+    private static final String PROCESSED_PUSH_KEY = "processed-push-events";
 
     public IdempotencyService(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -19,7 +22,37 @@ public class IdempotencyService {
         );
     }
 
+    public boolean isEmailProcessed(String eventId) {
+        return Boolean.TRUE.equals(
+                redisTemplate.opsForSet().isMember(PROCESSED_EMAIL_KEY, eventId)
+        );
+    }
+
+    public boolean isSmsProcessed(String eventId) {
+        return Boolean.TRUE.equals(
+                redisTemplate.opsForSet().isMember(PROCESSED_SMS_KEY, eventId)
+        );
+    }
+
+    public boolean isPushProcessed(String eventId) {
+        return Boolean.TRUE.equals(
+                redisTemplate.opsForSet().isMember(PROCESSED_PUSH_KEY, eventId)
+        );
+    }
+
     public void markProcessed(String eventId) {
         redisTemplate.opsForSet().add(PROCESSED_KEY, eventId);
+    }
+
+    public void markEmailProcessed(String eventId) {
+        redisTemplate.opsForSet().add(PROCESSED_EMAIL_KEY, eventId);
+    }
+
+    public void markSmsProcessed(String eventId) {
+        redisTemplate.opsForSet().add(PROCESSED_SMS_KEY, eventId);
+    }
+
+    public void markPushProcessed(String eventId) {
+        redisTemplate.opsForSet().add(PROCESSED_PUSH_KEY, eventId);
     }
 }
