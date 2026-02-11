f6739e271446a0ca7569be66f193f56f9ff44592
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-11 22:04:59 +0530

    updated publish method to include openTelemetry tracer id

--- api-gateway/src/main/java/com/example/apigateway/controller/NotificationController.java
+++ api-gateway/src/main/java/com/example/apigateway/controller/NotificationController.java
@@ -2,6 +2,10 @@ package com.example.apigateway.controller;
 
 import com.example.apigateway.dto.NotificationRequest;
 import com.example.apigateway.producer.NotificationEventProducer;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +22,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     private final NotificationEventProducer producer;
+    private final Tracer tracer;
+
+//    @PostMapping
+//    public ResponseEntity<Void> publish(@Valid @RequestBody NotificationRequest request){
+//        log.info("Received notification request for userId: {}", request.userId());
+//        producer.publish(request);
+//        log.info("Published notification event for userId: {}", request.userId());
+//        return ResponseEntity.accepted().build();
+//    }
 
     @PostMapping
-    public ResponseEntity<Void> publish(@Valid @RequestBody NotificationRequest request){
-        log.info("Received notification request for userId: {}", request.userId());
-        producer.publish(request);
-        log.info("Published notification event for userId: {}", request.userId());
-        return ResponseEntity.accepted().build();
+    public ResponseEntity<Void> publish(
+            @RequestBody NotificationRequest request) {
+
+        Span span = tracer.spanBuilder("POST /api/notifications")
+                .setSpanKind(SpanKind.SERVER)
+                .startSpan();
+
+        try (Scope scope = span.makeCurrent()) {
+            log.info("Received notification request for userId: {}", request.userId());
+            producer.publish(request);
+            log.info("Published notification event for userId: {}", request.userId());
+            return ResponseEntity.accepted().build();
+
+        } catch (Exception ex) {
+            span.recordException(ex);
+            throw ex;
+        } finally {
+            span.end();
+        }
     }
+
+
 }
