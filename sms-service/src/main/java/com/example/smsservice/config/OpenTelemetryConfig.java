38ab68365df504292f6e933949a8a2e281598ab1
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-12 12:15:54 +0530

    added global opentelemetry config

---
+++ sms-service/src/main/java/com/example/smsservice/config/OpenTelemetryConfig.java
@@ -0,0 +1,66 @@
+package com.example.smsservice.config;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.semconv.ServiceAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    public OpenTelemetry openTelemetry() {
+
+        Resource resource = Resource.getDefault()
+                .merge(Resource.create(
+                        Attributes.of(
+                                ServiceAttributes.SERVICE_NAME,
+                                "sms-service"
+                        )
+                ));
+
+        SdkTracerProvider tracerProvider =
+                SdkTracerProvider.builder()
+                        .addSpanProcessor(
+                                BatchSpanProcessor.builder(
+                                        OtlpGrpcSpanExporter.builder()
+                                                .setEndpoint("http://otel-collector:4317")
+                                                .build()
+                                ).build()
+                        )
+                        .setResource(resource)
+                        .build();
+
+        OpenTelemetrySdk sdk =
+                OpenTelemetrySdk.builder()
+                        .setTracerProvider(tracerProvider)
+                        .setPropagators(
+                                ContextPropagators.create(
+                                        W3CTraceContextPropagator.getInstance()
+                                )
+                        )
+                        .build();
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(tracerProvider::close)
+        );
+
+        return sdk;
+    }
+
+    @Bean
+    public Tracer tracer(OpenTelemetry openTelemetry) {
+        return openTelemetry.getTracer("sms-service");
+    }
+}
+
