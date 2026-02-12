410b98f54febd5275d40258c51b399749f251e27
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-12 12:08:04 +0530

    used restTemplate bean instead of reinitializing new instance

--- notification-dispatcher/src/main/java/com/example/notificationdispatcher/client/PreferenceClient.java
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/client/PreferenceClient.java
@@ -1,16 +1,21 @@
 package com.example.notificationdispatcher.client;
 
 import com.example.contract.UserPreferenceDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class PreferenceClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate; //Bean for this is defined in OpenTelemetryConfig
 
     public UserPreferenceDTO fetch(String userId) throws HttpServerErrorException {
+        log.info("RestTemplate instance in PreferenceClient: " + restTemplate);
         return restTemplate.getForObject(
                 "http://user-preferences-service:8081/api/preferences/" + userId,
                 UserPreferenceDTO.class
