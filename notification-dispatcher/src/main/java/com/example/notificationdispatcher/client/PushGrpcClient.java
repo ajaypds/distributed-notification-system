d040fe48bfca968bc1d5e4753130290762aea447
Ajay Prasad <ajay.prasad@outlook.in>
2026-02-12 11:59:58 +0530

    added Grpc interceptor in Push client

--- notification-dispatcher/src/main/java/com/example/notificationdispatcher/client/PushGrpcClient.java
+++ notification-dispatcher/src/main/java/com/example/notificationdispatcher/client/PushGrpcClient.java
@@ -2,10 +2,12 @@ package com.example.notificationdispatcher.client;
 
 import com.example.notification.proto.NotificationRequest;
 import com.example.notification.proto.NotificationServiceGrpc;
+import com.example.notificationdispatcher.grpc.GrpcClientTracingInterceptor;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
+import io.opentelemetry.api.OpenTelemetry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -22,10 +24,12 @@ public class PushGrpcClient {
 
     private final NotificationServiceGrpc.NotificationServiceBlockingStub stub;
 
-    public PushGrpcClient() {
+    public PushGrpcClient(OpenTelemetry openTelemetry) {
+        log.info("OpenTelemetry configured for PushGrpcClient: " + openTelemetry);
         ManagedChannel channel =
                 ManagedChannelBuilder.forAddress("push-service", 9090)
                         .usePlaintext()
+                        .intercept(new GrpcClientTracingInterceptor(openTelemetry))
                         .build();
 
         this.stub = NotificationServiceGrpc.newBlockingStub(channel);
